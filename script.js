c2c1710f0c88c5ca01c1b1565f050034b4da9f16
ShaikhAnis30 <109184307+ShaikhAnis30@users.noreply.github.com>
2023-07-20 14:17:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
 
+let bandNames = ['The Rolling Stones', 'Aerosmith', 'Led Zeppelin', 'The Beatles'];
+function sortBandNames(names) {
+      const articles = ['a', 'an', 'the'];
+      return names.sort((a, b) => {
+        const name1 = a.toLowerCase().replace(/^(the|an|a)\s+/, '');
+        const name2 = b.toLowerCase().replace(/^(the|an|a)\s+/, '');
+        return name1.localeCompare(name2);
+      });
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+      const sortedBandNames = sortBandNames(bandNames);
+      const bandList = document.getElementById('band');
+
+      sortedBandNames.forEach(name => {
+        const li = document.createElement('li');
+        li.textContent = name;
+        bandList.appendChild(li);
+      });
+    });
